c5877329a6e4883469b3fdeddbc1da6850e4c397
huangjiaqi <huangjiaqi0713@gmail.com>
2022-07-12 01:19:06 +0900

    新建项目1-1

---
+++  testWeb/src/cn/jp/test/Dog.java
@@ -0,0 +1,22 @@
+package cn.jp.test;
+
+public class Dog {
+	String breed;
+	int size;
+	String colour;
+	int age;
+	
+	void eat(){
+		
+	}
+	void run(){
+		
+	}
+	void sleep(){
+		
+	}
+	void name(){
+		
+	}
+ 
+}
